1074029d8b87609449ec9efdf5bed52941bb0feb
ChanduCh0604 <chchandrasekharreddy805@gmail.com>
2026-08-11 14:37:42 +0530

    Successfully configured of the git and eclipse

---
+++ CorejavaProject_batch77/src/com/corejava/GitTesting.java
@@ -0,0 +1,12 @@
+package com.corejava;
+public class GitTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi git");
+		System.out.println("hello");
+		
+		System.out.println("git testing");
+	}
+
+}
